82cdf143f42c6ebd27a49b4559df4ba6824d68ec
Juyeon Lee <yeonlee@ys-MacBookPro.local>
2021-06-25 23:23:07 +0900

    add near sign

---
+++ test/keystore/near.js
@@ -0,0 +1,60 @@
+const { JWE } = require("node-jose");
+const { mnemonicToSeedSync } = require("bip39");
+const { COIN } = require("../../lib");
+const near = require("../../lib/blockchains/near/keyStore");
+
+const {
+  createKeyStore,
+  getAccount,
+  getAlgo2HashKey,
+} = require("./_getAccount");
+
+const MNEMONIC = require("../mnemonic.json");
+
+const TYPE = COIN.NEAR;
+const INDEX = 1;
+
+async function getSeed(keyStore, password) {
+  const key = await getAlgo2HashKey(password, keyStore);
+  const mnemonic = await JWE.createDecrypt(key).decrypt(keyStore.j.join("."));
+  const seed = mnemonicToSeedSync(mnemonic.plaintext.toString());
+  return seed
+}
+
+async function signTx(seed, path) {
+  try {
+    const response = await near.KEYSTORE.signTx( 
+      seed,
+      path,
+      {
+        sender: "kms.testnet",
+        receiver: "receiver.testnet",
+        networkId: "testnet",
+        amount: "0.5",
+      }
+    );
+
+    console.log("response - ", response);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function run() {
+  const PASSWORD = MNEMONIC.password;
+  const keyStore = await createKeyStore(PASSWORD);
+  const SEED = await getSeed(
+    keyStore,
+    PASSWORD
+  );
+
+  await signTx(
+    SEED, 
+    { type: TYPE, account: 0, index: INDEX }
+  );
+}
+
+run();
+
+
