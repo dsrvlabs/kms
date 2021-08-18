9862a66bd5f986e884fc43fc497784c0ba718765
daoauth <57783762+daoauth@users.noreply.github.com>
2021-08-18 15:21:57 +0900

    ledger web ble support

--- src/blockchains/flow/keyStore.ts
+++ src/blockchains/flow/keyStore.ts
@@ -1,13 +1,12 @@
 import { BIP32Interface } from "bip32";
-
-const EC = require("elliptic").ec;
+import { ec as EC } from "elliptic";
 
 export class KEYSTORE {
   static getAccount(node: BIP32Interface): string {
     const { privateKey } = node;
     const ec = new EC("secp256k1");
-    const keyPair = ec.keyFromPrivate(privateKey);
-    return keyPair.getPublic().encode("hex", false).slice(2);
+    const keyPair = ec.keyFromPrivate(privateKey as Buffer);
+    return keyPair.getPublic().encode("hex", false); // .slice(2);
   }
   /*
   static signTx(node: BIP32Interface, rawTx: RawTx): SignedTx {
