a9a1a6ba33c929af86e9dbc9f79fb9532f5a4d71
seungwon2 <dandelionsw2@ewhain.net>
2021-09-03 10:33:07 +0900

    Chore : change output format

--- test/keystore/celo.js
+++ test/keystore/celo.js
@@ -14,9 +14,12 @@ async function signTx(path, keyStore, password, account) {
   try {
     const mnemonic = await getMnemonic(password, keyStore);
     const response = await signTxFromKeyStore(path, mnemonic, {
-      nonce: "0x06",
+      nonce: "0x10",
       gasPrice: "0x09184e72a000",
       gasLimit: "0x9710",
+      feeCurrency: "",
+      gatewayFeeRecipient: "",
+      gatewayFee: "",
       to: "0xdcb6702936a4C257c7e715BF780925a93B217e37",
       value: "0x00",
       data: "0x7f7465737432000000000000000000000000000000000000000000000000000000600057",
