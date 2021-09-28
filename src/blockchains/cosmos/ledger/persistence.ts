58912c90371d89bcb2265784171c4f40f6aef15a
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2021-09-28 17:27:43 +0900

    add persistence ledger

---
+++ src/blockchains/cosmos/ledger/persistence.ts
@@ -0,0 +1,43 @@
+import Transport from "@ledgerhq/hw-transport";
+import * as secp256k1 from "secp256k1";
+import { BIP44, RawTx, SignedTx } from "../../../types";
+
+const CosmosApp = require("ledger-cosmos-js").default;
+
+// LEDGER
+export class LEDGER {
+  static async getAccount(path: BIP44, transport: Transport): Promise<string> {
+    const instance = new CosmosApp(transport);
+    const response = await instance.getAddressAndPubKey(
+      [44, path.type, path.account, 0, path.index],
+      "persistence"
+    );
+    return response.bech32_address;
+  }
+
+  static async signTx(
+    path: BIP44,
+    transport: Transport,
+    rawTx: RawTx
+  ): Promise<SignedTx> {
+    const instance = new CosmosApp(transport);
+    const response = await instance.sign(
+      [44, path.type, path.account, 0, path.index],
+      JSON.stringify({ ...rawTx })
+    );
+    // TODO: test
+    const signature = secp256k1.signatureImport(
+      Buffer.from(response.signature)
+    );
+    return { rawTx, signedTx: { signature } };
+  }
+
+  /*
+  export function signMessage(
+    path: BIP44,
+    transport: Transport,
+    msg: string) {
+    // ...
+  }
+  */
+}
